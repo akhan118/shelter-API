b8752bfb6c030ba85a83bed4f3aefbd2b6e423f7
Ahmad Khan <ahmad.khan@cusolutionsgroup.com>
2018-02-10 15:22:34 -0500

    userTable update
    UserTable Update

--- common/models/User.php
+++ common/models/User.php
@@ -69,7 +69,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+      return static::findOne(['auth_key' => $token]);
     }
 
     /**
