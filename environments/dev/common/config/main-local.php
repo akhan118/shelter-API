bea6f22f5850d213640f35b8825c88ebe5ee4876
Shaamala <shaamalajabbar@gmail.com>
2018-08-11 23:16:21 -0400

    add authManager

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=homeless_project',
+            'dsn' => 'mysql:host=localhost:3307;dbname=homeless',
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8',
